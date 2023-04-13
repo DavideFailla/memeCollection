7029ecebff9dab9ae990d8650b560cdd727f2353
DavideFailla <davide.failla4@gmail.com>
2023-04-13 12:20:42 +0200

    first commit

---
+++ src/main/java/org/generation/italy/memeCollection/MemeCollectionApplication.java
@@ -0,0 +1,13 @@
+package org.generation.italy.memeCollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemeCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/generation/italy/memeCollection/MemeCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.italy.memeCollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
